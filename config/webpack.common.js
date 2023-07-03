b0e6be6f687d824e622a2abfd25150ee7fc53848
An Le <an.le@i-on.net>
2023-07-03 09:41:44 +0700

    init project

---
+++ config/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          }
+        }
+      },
+    ]
+  },
+  output: {
+    /**
+     * With zero configuration,
+     *   clean-webpack-plugin will remove files inside the directory below
+     */
+    path: path.resolve(process.cwd(), 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new CleanWebpackPlugin(),
+  ]
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,34 @@
+const {merge} = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const packageJson = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: '/index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'map',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MapApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
