18281b861fe13c9a49e35e005e4c0405bdae82be
An Le <an.le@i-on.net>
2023-07-03 10:50:23 +0700

    pass options to render

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const mount = (el) => {
+const mount = (el, options) => {
   const root = ReactDOM.createRoot(el);
   root.render(
     <React.StrictMode>
-      <App />
+      <App {...options}/>
     </React.StrictMode>
   );
 };
